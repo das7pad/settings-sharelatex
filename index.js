4a707f9b0c285472d7822972abf8b951ad3acd31
Jakob Ackermann <das7pad@outlook.com>
2020-11-26 23:23:29 +0100

    [misc] decaffeinate and format using the options of the redis module

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
-fs = require "fs"
-path = require "path"
-env = (process.env.NODE_ENV or "development").toLowerCase()
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+let possibleConfigFiles
+const fs = require('fs')
+const path = require('path')
+const env = (process.env.NODE_ENV || 'development').toLowerCase()
 
-merge = (settings, defaults) ->
-	for key, value of settings
-		if typeof(value) == "object" and value not instanceof Array
-			defaults[key] = merge(settings[key], defaults[key] or {})
-		else
-			defaults[key] = value
-	return defaults
+var merge = function (settings, defaults) {
+  for (const key in settings) {
+    const value = settings[key]
+    if (typeof value === 'object' && !(value instanceof Array)) {
+      defaults[key] = merge(settings[key], defaults[key] || {})
+    } else {
+      defaults[key] = value
+    }
+  }
+  return defaults
+}
 
-possibleDefaultSettingsPaths = [
-	path.normalize(__dirname + "/../../config/settings.defaults"),
-	path.normalize(process.cwd() + "/config/settings.defaults"),
+const possibleDefaultSettingsPaths = [
+  path.normalize(__dirname + '/../../config/settings.defaults'),
+  path.normalize(process.cwd() + '/config/settings.defaults'),
 ]
 
-defaults = {}
-settingsExist = false
+let defaults = {}
+let settingsExist = false
 
-for defaultSettingsPath in possibleDefaultSettingsPaths
-	if fs.existsSync("#{defaultSettingsPath}.coffee") or fs.existsSync("#{defaultSettingsPath}.js")
-		defaults = require(defaultSettingsPath)
-		settingsExist = true
-		break
+for (const defaultSettingsPath of Array.from(possibleDefaultSettingsPaths)) {
+  if (
+    fs.existsSync(`${defaultSettingsPath}.coffee`) ||
+    fs.existsSync(`${defaultSettingsPath}.js`)
+  ) {
+    defaults = require(defaultSettingsPath)
+    settingsExist = true
+    break
+  }
+}
 
-if process.env.SHARELATEX_CONFIG?
-	possibleConfigFiles = [process.env.SHARELATEX_CONFIG]
-else
-	possibleConfigFiles = [
-		process.cwd() + "/config/settings.#{env}.js"
-		path.normalize(__dirname + "/../../config/settings.#{env}.js")
-		process.cwd() + "/config/settings.#{env}.coffee"
-		path.normalize(__dirname + "/../../config/settings.#{env}.coffee")
-	]
+if (process.env.SHARELATEX_CONFIG != null) {
+  possibleConfigFiles = [process.env.SHARELATEX_CONFIG]
+} else {
+  possibleConfigFiles = [
+    process.cwd() + `/config/settings.${env}.js`,
+    path.normalize(__dirname + `/../../config/settings.${env}.js`),
+    process.cwd() + `/config/settings.${env}.coffee`,
+    path.normalize(__dirname + `/../../config/settings.${env}.coffee`),
+  ]
+}
 
-for file in possibleConfigFiles
-	if fs.existsSync(file)
-		module.exports = merge(require(file), defaults)
-		settingsExist = true
-		break
+for (const file of Array.from(possibleConfigFiles)) {
+  if (fs.existsSync(file)) {
+    module.exports = merge(require(file), defaults)
+    settingsExist = true
+    break
+  }
+}
 
-if !settingsExist
-	console.warn "No settings or defaults found. I'm flying blind."
+if (!settingsExist) {
+  console.warn("No settings or defaults found. I'm flying blind.")
+}
 
 module.exports = defaults
